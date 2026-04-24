cc4b6539ef718abd780c28a46033a6fd4fdf5439
keith <2257761605@qq.com>
2026-04-24 18:06:11 +0800

    add lekiwi_base

---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/lekiwi_controller/lekiwi_controller_env_cfg.py
@@ -0,0 +1,242 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import math
+
+import isaaclab.sim as sim_utils
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg
+from isaaclab.envs import ManagerBasedRLEnvCfg
+from isaaclab.managers import EventTermCfg as EventTerm
+from isaaclab.managers import ObservationGroupCfg as ObsGroup
+from isaaclab.managers import ObservationTermCfg as ObsTerm
+from isaaclab.managers import RewardTermCfg as RewTerm
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.managers import TerminationTermCfg as DoneTerm
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sensors import ContactSensorCfg
+from isaaclab.utils import configclass
+from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
+from . import mdp
+
+##
+# Pre-defined configs
+##
+
+from isaaclab_assets.robots.cartpole import CARTPOLE_CFG  # isort:skip
+
+
+##
+# Scene definition
+##
+
+
+@configclass
+class LekiwiControllerSceneCfg(InteractiveSceneCfg):
+    """Configuration for a cart-pole scene."""
+
+    # ground plane
+    ground = AssetBaseCfg(
+        prim_path="/World/ground",
+        spawn=sim_utils.GroundPlaneCfg(size=(100.0, 100.0)),
+    )
+
+    # robot
+    robot: ArticulationCfg = ArticulationCfg(
+        prim_path="{ENV_REGEX_NS}/Robot",
+        spawn=sim_utils.UsdFileCfg(
+            usd_path="/home/synapath/isaac/new_robot/lekiwi_controller/lekiwi/lekiwi.usd",
+            activate_contact_sensors=True,      #开启碰撞传感器
+            rigid_props=sim_utils.RigidBodyPropertiesCfg(
+                disable_gravity=False,      # 开启重力
+                retain_accelerations=False, #不保留加速度
+                linear_damping=0.0,         #直线阻尼
+                angular_damping=0.0,
+                max_depenetration_velocity=1.0, #最大穿摸速度
+            ),
+            
+            articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+                enabled_self_collisions=False,      #关闭自碰撞
+                solver_position_iteration_count=4,  #位置求解迭代次数,不需要
+                solver_velocity_iteration_count=2,  #速度求解迭代次数
+            ),
+        ),
+        init_state=ArticulationCfg.InitialStateCfg(
+            pos=(0.0, 0.0, 0.2), # 初始高度 20cm
+            rot=(1.0, 0.0, 0.0, 0.0),
+            joint_pos={".*": 0.0},
+        ),
+        actuators={
+            "wheels": ImplicitActuatorCfg(
+                joint_names_expr=[".*wheel_drive"],
+                effort_limit=2.0,
+                velocity_limit_sim=4.717,
+                stiffness=0.0,  # p
+                damping=10.0,    # d
+            )
+        },
+    )
+    contact_forces = ContactSensorCfg(
+        prim_path="{ENV_REGEX_NS}/Robot/base_link", 
+        update_period=0.0,  #实时更新
+        history_length=3,   #保留最近3次接触力数据
+        debug_vis=True,
+    )
+    # lights
+    dome_light = AssetBaseCfg(
+        prim_path="/World/DomeLight",
+        spawn=sim_utils.DomeLightCfg(color=(0.9, 0.9, 0.9), intensity=500.0),
+    )
+
+
+##
+# MDP settings
+##
+
+
+@configclass
+class ActionsCfg:
+    """Action specifications for the MDP."""
+    joint_velocity = mdp.JointVelocityActionCfg(
+        asset_name="robot",
+        joint_names=[".*wheel_drive"],
+        scale=1.0,
+    )
+@configclass
+class CommandsCfg:
+    base_velocity = mdp.UniformVelocityCommandCfg(
+        class_type=mdp.UniformVelocityCommand,
+        resampling_time_range=(5.0, 8.0),
+        debug_vis=True,
+        asset_name="robot",
+        ranges=mdp.UniformVelocityCommandCfg.Ranges(
+            lin_vel_x=(-2.0, 2.0),
+            lin_vel_y=(-2.0, 2.0),
+            ang_vel_z=(-2.0, 2.0),
+        ),
+    )
+
+@configclass
+class ObservationsCfg:
+    """Observation specifications for the MDP."""
+
+    @configclass
+    class PolicyCfg(ObsGroup):
+        """Observations for policy group."""
+        base_lin_vel = ObsTerm(
+            func=mdp.base_lin_vel,
+            noise=Unoise(n_min=-0.1, n_max=0.1),  # 均匀噪声 ±0.1 m/s
+        )
+        # 基座角速度
+        base_ang_vel = ObsTerm(
+            func=mdp.base_ang_vel,
+            noise=Unoise(n_min=-0.2, n_max=0.2),  # 角速度噪声更大
+        )
+        # 重力投影向量（用于感知姿态倾斜）
+        projected_gravity = ObsTerm(
+            func=mdp.projected_gravity,
+            noise=Unoise(n_min=-0.05, n_max=0.05),
+        )
+        # 目标速度命令
+        velocity_commands = ObsTerm(
+            func=mdp.generated_commands,
+            params={"command_name": "base_velocity"}
+        )
+        # 关节速度
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Unoise(n_min=-1.5, n_max=1.5))
+
+        def __post_init__(self) -> None:
+            self.enable_corruption = False
+            self.concatenate_terms = True
+
+    # observation groups
+    policy: PolicyCfg = PolicyCfg()
+
+
+@configclass
+class EventCfg:
+    """Configuration for events."""
+
+    reset_base = EventTerm(
+        func=mdp.reset_root_state_uniform,
+        mode="reset",
+        params={
+            "asset_cfg": SceneEntityCfg("robot"),
+            "position_range": (-0.5, 0.5, 0.15, 0.25),  # xy 随机位置
+            "orientation_range": (-0.1, 0.1, -0.1, 0.1), # 轻微随机角度
+            "velocity_range": (-0.1, 0.1, -0.1, 0.1),
+        },
+    )
+
+
+@configclass
+class RewardsCfg:
+    # 跟踪直线速度（最重要）
+    track_lin_vel_xy_exp = RewTerm(
+        func=mdp.track_lin_vel_xy_exp,
+        weight=1.5,
+        params={"command_name": "base_velocity", "std": math.sqrt(0.25)},
+    )
+    # 跟踪旋转速度
+    track_ang_vel_z_exp = RewTerm(
+        func=mdp.track_ang_vel_z_exp,
+        weight=1.0,
+        params={"command_name": "base_velocity", "std": math.sqrt(0.25)},
+    )
+    # 活着奖励
+    # alive = RewTerm(func=mdp.is_alive, weight=1.0)
+    # 失败惩罚
+    terminating = RewTerm(func=mdp.is_terminated, weight=-2.0)
+
+
+@configclass
+class TerminationsCfg:
+    """Termination terms for the MDP."""
+
+    # (1) Time out
+    time_out = DoneTerm(func=mdp.time_out, time_out=True)
+    # (2) Cart out of bounds
+    # cart_out_of_bounds = DoneTerm(
+    #     func=mdp.joint_pos_out_of_manual_limit,
+    #     params={"asset_cfg": SceneEntityCfg("robot", joint_names=["slider_to_cart"]), "bounds": (-3.0, 3.0)},
+    # )
+    illegal_contact_term = DoneTerm(
+        func=mdp.illegal_contact,
+        params={
+            "sensor_cfg": SceneEntityCfg("contact_forces"), 
+            "threshold": 10.0, 
+        },
+    )
+
+
+##
+# Environment configuration
+##
+
+
+@configclass
+class LekiwiControllerEnvCfg(ManagerBasedRLEnvCfg):
+    # Scene settings
+    scene: LekiwiControllerSceneCfg = LekiwiControllerSceneCfg(num_envs=8, env_spacing=4.0)
+    # Basic settings
+    observations: ObservationsCfg = ObservationsCfg()
+    actions: ActionsCfg = ActionsCfg()
+    commands: CommandsCfg = CommandsCfg()
+    events: EventCfg = EventCfg()
+    # MDP settings
+    rewards: RewardsCfg = RewardsCfg()
+    terminations: TerminationsCfg = TerminationsCfg()
+
+    # Post initialization
+    def __post_init__(self) -> None:
+        """Post initialization."""
+        # general settings
+        self.decimation = 4
+        self.episode_length_s = 8
+        # viewer settings
+        self.viewer.eye = (8.0, 0.0, 5.0)
+        # simulation settings   
+        self.sim.dt = 1 / 120
+        self.sim.render_interval = self.decimation
